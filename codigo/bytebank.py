6dd69411834268e5d68a4114f80dde35cecf00f0
violiveiradev <violiveira.dev@gmail.com>
2023-03-26 15:11:15 -0300

    Test-Driven Development

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -19,6 +19,20 @@ class Funcionario:
         ano_nascimento = data_nascimento_quebrada[-1]
         ano_atual = date.today().year
         return ano_atual - int(ano_nascimento)
+    
+    def sobrenome(self):
+        nome_completo = self.nome.strip()
+        nome_quebrado = nome_completo.split(' ')
+        return nome_quebrado[-1]
+    
+    def _eh_socio(self):
+        sobrenomes = ['Bragança', 'Wndsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu', 'Oliveira']
+        return (self._salario >= 100000) and (self.sobrenome() in sobrenomes)
+    
+    def decrescimo_salario(self):
+        if self._eh_socio():
+            decrescimo = self._salario * 0.1
+            self._salario = self._salario - decrescimo
 
     def calcular_bonus(self):
         valor = self._salario * 0.1
---
+++ tests/test_bytebank.py
@@ -0,0 +1,32 @@
+from codigo.bytebank import Funcionario
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+        entrada = '13/03/2000' # Given-Contexto
+        esperado = 23
+
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.idade() # When-Ação
+
+        assert resultado == esperado  # Then-Desfecho
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = ' Lucas Carvalho ' # Given
+        esperado = 'Carvalho'
+
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome() # When
+
+        assert resultado == esperado # Then
+
+    def test_quando_decrecimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000 # Given
+        entrada_nome = 'Paulo Bragança'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario() # When
+        resultado = funcionario_teste.salario 
+
+        assert resultado == esperado # Then
+
